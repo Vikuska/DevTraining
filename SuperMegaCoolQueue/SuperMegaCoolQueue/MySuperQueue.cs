198e81fb10d31ff3f51c14db89d46821863497ac
Vikuska <mihailova.vika@gmail.com>
2014-12-03 13:42:15 +0200

    Queue

---
+++ SuperMegaCoolQueue/SuperMegaCoolQueue/MySuperQueue.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace SuperMegaCoolQueue
+{
+    class ListNode<T>
+    {
+        public T Data { get; set; }
+        public ListNode<T> Next { get; set; }
+
+        public ListNode(T data)
+        {
+            Data = data;
+        }
+    }
+
+    internal class MySuperQueue<T>
+    {
+        private ListNode<T> _first;
+        private ListNode<T> _last;
+        private int size;
+        public int Size
+        {
+            get { return size; }
+        }
+
+        public MySuperQueue()
+        {
+            _first = new ListNode<T>(default(T));
+            _last = _first;
+            size = 0;
+        }
+
+        public T FirstElement()
+        {
+            if (Empty())
+                Console.WriteLine("Queue is empty");
+            return _first.Data;
+        }
+
+        public T LastElement()
+        {
+            if (Empty())
+                Console.WriteLine("Queue is empty");
+            return _last.Data;
+        }
+
+        private bool Empty()
+        {
+            return (size == 0) || (_first == null);
+        }
+
+        public void Enqueu(T item)
+        {
+            ListNode<T> newItem = new ListNode<T>(item);
+
+            if (Empty())
+                _first = newItem;
+
+            _last.Next = newItem;
+            _last = newItem;
+
+            size++;
+        }
+
+        public void Dequeu()
+        {
+            if (Empty())
+                throw new NullReferenceException();
+            _first = _first.Next ?? _last;
+            size--;
+        }
+    }
+}
---
+++ SuperMegaCoolQueue/SuperMegaCoolQueue/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMegaCoolQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MySuperQueue<int> q = new MySuperQueue<int>();
+            q.Enqueu(1);
+            q.Enqueu(2);
+            q.Enqueu(3);
+            Console.WriteLine("Size = {0}", q.Size);
+            Console.WriteLine("FirstElement = {0}", q.FirstElement());
+            Console.WriteLine("LastElement = {0}", q.LastElement());
+
+            q.Dequeu();
+            Console.WriteLine("Size = {0}", q.Size);
+            Console.WriteLine("FirstElement = {0}", q.FirstElement());
+
+            Console.ReadLine();
+        }
+    }
+}
